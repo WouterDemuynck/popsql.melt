e536292ef1206c8de1ea1b504ebd908ce85d3a2b
Wouter Demuynck <wouter.am.demuynck@gmail.com>
2013-11-02 11:42:38 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+popsql.melt
+======
+
+Popsql Melt is a micro-ORM based on the Popsql library focusing on maintainability and flexibility.
---
+++ src/Popsql.Melt/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Popsql.Melt")]
+[assembly: AssemblyDescription("Popsql Melt is a micro-ORM based on the Popsql library focusing on maintainability and flexibility.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Popsql")]
+[assembly: AssemblyCopyright("Copyright © 2013 Wouter Demuynck")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("96d5b041-2ca4-4962-8ba5-86d3a8308011")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0.0")]
